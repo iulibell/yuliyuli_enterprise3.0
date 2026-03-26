7d5cf9a46499cf843c0c09de12d325b4e9c397b7
Dima-Yulia <3427293417@qq.com>
2026-03-26 16:15:49 +0800

    done?

--- src/main/java/com/yuliyuli/dto/vo/LoginVO.java
+++ src/main/java/com/yuliyuli/dto/vo/LoginVO.java
@@ -2,16 +2,12 @@ package com.yuliyuli.dto.vo;
 
 import com.yuliyuli.entity.user.User;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class LoginVO {
-  @Schema(description = "登录凭证")
   private String token;
-
-  @Schema(description = "用户信息")
   private User user;
 }
